17cc4363fc2a5eaf74b79907f601fd3aea4d6463
Souravkumar0111 <142584778+Souravkumar0111@users.noreply.github.com>
2025-01-30 00:18:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,7 @@ import dotenv from'dotenv';
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT||5000;
-app.use(cors(
-
-    {
-        origin:["https://f-oodon-02-oin2-frontend.vercel.app"],
-        methods:["POST","GET"],
-        credentials: true
-
-    }
-    
-));
+app.use(cors());
 // app.use((req,res,next)=>{
 //     res.setHeader("Access-Control-Allow-Origin","http://localhost:5173");
 //     res.header(
